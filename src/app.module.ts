bea623a28d2dbb7842bda1adc9b93906d072d70c
Wojciech Dlugosz <webmaster@styronet.pl>
2024-12-17 17:22:57 +0100

    Init NestJS app, create and init db, prisma, add models for products and orders

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
+import * as cors from 'cors';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(cors()).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api');
+  await app.enableShutdownHooks();
+  await app.listen(3000);
+}
+bootstrap();
