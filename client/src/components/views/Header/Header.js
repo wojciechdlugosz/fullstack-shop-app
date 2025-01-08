87f3143b43617b011df107e0d8390bb1970a21ba
Wojciech Dlugosz <webmaster@styronet.pl>
2025-01-08 16:20:44 +0100

    Add number of cart products to cart icon in header

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -2,30 +2,42 @@ import Navigation from '../../features/Navigation/Navigation';
 import styles from './Header.module.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { BsBagPlusFill } from 'react-icons/bs';
+import { getAllCartProducts } from '../../../redux/cartRedux';
+import { useSelector } from 'react-redux';
 
-const Header = () => (
-  <header className={styles.header}>
-    <Link to="/">
-      <img
-        className={styles.header__logo}
-        src={`${process.env.PUBLIC_URL}/images/logo-najlepszystyropian.png`}
-        alt="Najlepszy styropian logo"
-      />
-    </Link>
-    <div className={styles.header__navbar}>
-      <Navigation />
-      <NavLink
-        className={({ isActive }) =>
-          isActive
-            ? styles.header__navbar__cartIcon___active
-            : styles.header__navbar__cartIcon
-        }
-        to="/cart"
-      >
-        <BsBagPlusFill />
-      </NavLink>
-    </div>
-  </header>
-);
+const Header = () => {
+  const cartProducts = useSelector(getAllCartProducts);
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <img
+          className={styles.header__logo}
+          src={`${process.env.PUBLIC_URL}/images/logo-najlepszystyropian.png`}
+          alt="Najlepszy styropian logo"
+        />
+      </Link>
+      <div className={styles.header__navbar}>
+        <Navigation />
+        <NavLink
+          className={({ isActive }) =>
+            isActive
+              ? styles.header__navbar__cartIcon___active
+              : styles.header__navbar__cartIcon
+          }
+          to="/cart"
+        >
+          <BsBagPlusFill />
+          {cartProducts.length > 0 ? (
+            <span className={styles.header__navbar__cartIcon___amount}>
+              {cartProducts.length}
+            </span>
+          ) : (
+            ''
+          )}
+        </NavLink>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
