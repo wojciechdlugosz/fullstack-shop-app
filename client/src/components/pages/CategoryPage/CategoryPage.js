9c3852538047fa14e6ed655ec93980c60f8fd6de
Wojciech Dlugosz <webmaster@styronet.pl>
2025-01-10 14:18:57 +0100

    Add category title on category page

--- client/src/components/pages/CategoryPage/CategoryPage.js
+++ client/src/components/pages/CategoryPage/CategoryPage.js
@@ -11,18 +11,27 @@ import {
   getRequests,
 } from '../../../redux/productsRedux';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 const CategoryPage = () => {
   const { category } = useParams();
-  //console.log(category);
   const categories = useSelector(getAllCategories);
-  //console.log(categories);
   const categoryProducts = useSelector((state) =>
     getProductsByCategory(state, category),
   );
   const allProducts = useSelector(getAllProducts);
   const requests = useSelector(getRequests);
   const request = requests['app/products/DATA_PRODUCTS'];
+
+  const [categoryName, setCategoryName] = useState('');
+
+  useEffect(() => {
+    if (category === 'all') {
+      setCategoryName('wszystkie produkty');
+    } else {
+      setCategoryName(categories.find((cat) => cat.name === category).text);
+    }
+  }, [category, categories]);
 
   if (
     !categories.some((cat) => cat.name === category) &&
@@ -32,9 +41,7 @@ const CategoryPage = () => {
   }
 
   return (
-    
     <section className={styles.showcase}>
-
       <aside className={styles.showcase__nav}>
         <SideNavigation />
       </aside>
@@ -43,26 +50,29 @@ const CategoryPage = () => {
         <p>O nie, coś poszło nie tak :( Spróbuj później</p>
       )}
       {request && request.success && (
-        <div className={styles.showcase__elements}>
-          {category === 'all'
-            ? allProducts.map(({ id, main_img, name, price }) => (
-                <ProductSummary
-                  key={id}
-                  id={id}
-                  img={main_img}
-                  name={name}
-                  price={price}
-                />
-              ))
-            : categoryProducts.map(({ id, main_img, name, price }) => (
-                <ProductSummary
-                  key={id}
-                  id={id}
-                  img={main_img}
-                  name={name}
-                  price={price}
-                />
-              ))}
+        <div className={styles.showcase__products}>
+          <h1 className={styles.showcase__products__title}>{categoryName}</h1>
+          <div className={styles.showcase__products__elements}>
+            {category === 'all'
+              ? allProducts.map(({ id, main_img, name, price }) => (
+                  <ProductSummary
+                    key={id}
+                    id={id}
+                    img={main_img}
+                    name={name}
+                    price={price}
+                  />
+                ))
+              : categoryProducts.map(({ id, main_img, name, price }) => (
+                  <ProductSummary
+                    key={id}
+                    id={id}
+                    img={main_img}
+                    name={name}
+                    price={price}
+                  />
+                ))}
+          </div>
         </div>
       )}
     </section>
